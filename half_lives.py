203332d3879bc294d6a3a54859e8d6cdff7a7fe4
Mohammad Hayajneh <35841393+mohammadhayajneh@users.noreply.github.com>
2018-02-17 09:23:41 -0500

    Add files via upload

---
+++ half_lives.py
@@ -0,0 +1,9 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> half_lives = [887.7, 24100.0, 6563.0, 14, 373300.0]
+>>> for i in half_lives:
+	print (i,end=' ')
+
+	
+887.7 24100.0 6563.0 14 373300.0 
+>>> 
